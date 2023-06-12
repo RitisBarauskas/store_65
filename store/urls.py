bd1cb6493cd92c1fb260d184fd8be5bd37c9ad26
Ritis Barauskas <perm300ru@yandex.ru>
2023-06-12 17:47:06 +0300

    upd project

--- store/urls.py
+++ store/urls.py
@@ -1,24 +1,33 @@
-"""store URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import path, include, reverse_lazy
+from django.views.generic import CreateView
+
+
+auth_patterns = [
+    path('', include('django.contrib.auth.urls')),
+    path(
+        'registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('movies:main'),
+        ),
+        name='registration',
+    ),
+]
 
 urlpatterns = [
-    path('', include('movies.urls', namespace='movies')),
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include(auth_patterns)),
+    path('', include('movies.urls', namespace='movies')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
